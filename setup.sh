d8883766fa7fc3af515f566f1b482f696ed003eb
Ionut Calofir <calofir.ionut@yahoo.com>
2019-11-11 00:30:06 +0200

    Update dotfiles

--- setup.sh
+++ setup.sh
@@ -1,95 +1,44 @@
-#!/usr/bin/env bash
+sudo apt install git
+git clone https://github.com/ICalofir/dotfiles.git
 
-# install packages
-function install_packages() {
-    echo "Installing packages..." | tee -a logger.log
 
-    sudo apt -y update | tee -a logger.log
+sudo apt install vim
+sudo apt install tmux
+sudo apt install zsh
 
-    sudo apt-get -y install \
-        vim \
-        git \
-        tmux \
-        zsh \
-        curl \
-        | tee -a logger.log
+sudo apt install curl
+sudo apt install build-essential cmake
+sudo apt install clang
+sudo apt install fonts-powerline
 
-    sudo apt-get install software-properties-common | tee -a logger.log # be able to use add-apt-repository
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt update
+sudo apt install neovim
+sudo apt install python3-dev python3-pip
+sudo pip3 install neovim
 
-    sudo add-apt-repository ppa:neovim-ppa/stable | tee -a logger.log # add neovim package
-    sudo apt-get update | tee -a logger.log
+ln -sf "$HOME/Documents/Repositories/dotfiles/home/.tmux.conf" "$HOME/.tmux.conf"
 
-    sudo apt-get install neovim | tee -a logger.log # install neovim
-    sudo apt-get install python-dev python-pip python3-dev python3-pip | tee -a logger.log # prerequisites for the python modules
-    sudo pip3 install neovim | tee -a logger.log # vim python plugins with neovim
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+mv ~/.zshrc ~/.zshrc.pre-oh-my-zsh 
+ln -sf "$HOME/Documents/Repositories/dotfiles/home/.zshrc" "$HOME/.zshrc"
+ln -sf "$HOME/Documents/Repositories/dotfiles/home/zshrc" "$HOME/zshrc"
+sudo chsh -s $(which zsh)
 
-    # use neovim instead of vim
-    sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60 | tee -a logger.log
-    sudo update-alternatives --config vim | tee -a logger.log
+ln -sf "$HOME/Documents/Repositories/dotfiles/home/.vim" "$HOME/.vim"
+ln -sf "$HOME/Documents/Repositories/dotfiles/home/.vimrc" "$HOME/.vimrc"
+# YCM trebuie instalat separat dupa (momentan este comentat in .vimrc)
 
-    # other packages
-    sudo apt-get install build-essential cmake | tee -a logger.log
-    sudo apt-get install clang | tee -a logger.log
-    sudo apt-get install fonts-powerline | tee -a logger.log
+mkdir ~/.config/nvim
+ln -sf "$HOME/Documents/Repositories/dotfiles/home/.config/nvim/init.vim" "$HOME/.config/nvim/init.vim"
+vim +PlugClean! +qall
+vim +PlugInstall! +qall
+sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --config vim
+sudo pip3 install --upgrade pynvim
 
-    echo "Done installing packages!" | tee -a logger.log
-}
+ln -sf "$HOME/Documents/Repositories/dotfiles/home/.ctags" "$HOME/.ctags"
 
-function make_symlinks() {
-    echo "Making symlinks..." | tee -a logger.log
-    rm -rf ~/.vimrc
-    rm -rf ~/.zshrc
-    rm -rf ~/.tmux.conf
-    rm -rf ~/.ctags
-    rm -rf ~/.vim
-    rm -rf ~/zshrc
-    rm -rf ~/.config/nvim/init.vim
-
-    ln -sf "$HOME/Repositories/dotfiles/home/.vimrc" "$HOME/.vimrc" | tee -a logger.log
-    ln -sf "$HOME/Repositories/dotfiles/home/.zshrc" "$HOME/.zshrc" | tee -a logger.log
-    ln -sf "$HOME/Repositories/dotfiles/home/.tmux.conf" "$HOME/.tmux.conf" | tee -a logger.log
-    ln -sf "$HOME/Repositories/dotfiles/home/.ctags" "$HOME/.ctags" | tee -a logger.log
-    ln -sf "$HOME/Repositories/dotfiles/home/.vim" "$HOME/.vim" | tee -a logger.log
-    ln -sf "$HOME/Repositories/dotfiles/home/zshrc" "$HOME/zshrc" | tee -a logger.log
-    ln -sf "$HOME/Repositories/dotfiles/home/.config/nvim/init.vim" "$HOME/.config/nvim/init.vim" | tee -a logger.log
-    echo "Done making symlinks!" | tee -a logger.log
-}
-
-# install plugins
-function install_plugins() {
-    echo "Installing plugins..." | tee -a logger.log
-
-    # install oh-my-zsh
-    if [ -e ~/.oh-my-zsh ]; then
-        rm -rf ~/.oh-my-zsh
-    fi
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    rm -rf ~/.zshrc
-    mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
-
-    # install tmux plugin manager
-    if [ -e ~/.tmux/plugins/tpm ]; then
-        rm -rf ~/.tmux/plugins/tpm
-    fi
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm | tee -a logger.log
-    tmux source ~/.tmux.conf
-    ~/.tmux/plugins/tpm/bin/update_plugins all
-
-    # Plug allows post-update hooks
-    vim +PlugClean! +qall
-    vim +PlugInstall! +qall
-
-    # change shell to zsh
-    chsh -s $(which zsh)
-    sudo chsh -s $(which zsh)
-
-    echo "Done installing plugins!" | tee -a logger.log
-}
-
-echo "Installing packages..." | tee -a logger.log
-rm -rf logger.log
-git pull
-install_packages
-make_symlinks
-install_plugins
-echo "Done!" | tee -a logger.log
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+~/.tmux/plugins/tpm/bin/update_plugins all
+tmux source ~/.tmux.conf
