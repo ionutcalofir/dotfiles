1ae19b6cee9481b5dd1d4cdae54a70b02fdb75d5
ICalofir <calofir.ionut@yahoo.com>
2022-09-24 23:52:06 +0200

    Update dotfiles

--- build_nvim_from_source.sh
+++ build_nvim_from_source.sh
@@ -1,3 +1,5 @@
+sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+
 cd ~
 sudo rm /usr/local/bin/nvim
 sudo rm -r /usr/local/share/nvim/
@@ -5,13 +7,11 @@ sudo rm -rf neovim
 
 git clone https://github.com/neovim/neovim
 cd neovim
+git checkout nightly
 
+make distclean
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
 
 cd ~
 sudo rm -rf neovim
-
-# Steps:
-# - nvim
-# - :PlugUpdate
