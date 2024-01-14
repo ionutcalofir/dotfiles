dafaf3d73135b27c18f4d06c52b9ff596f3dcbe1
ionutcalofir <ionut.calofir@gmail.com>
2024-01-14 01:39:15 +0100

    Update dotfiles

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,6 +1,6 @@
+require('icalofir.plugins')
 require('icalofir.settings')
 require('icalofir.mappings')
-require('icalofir.plugins')
 require('icalofir.plugins_settings.nvim_cmp')
 require('icalofir.plugins_settings.treesitter')
 require('icalofir.plugins_settings.indent_guides')
--- home/.config/nvim/lua/icalofir/plugins_settings/nvim_cmp.lua
+++ home/.config/nvim/lua/icalofir/plugins_settings/nvim_cmp.lua
@@ -65,7 +65,7 @@ local on_attach = function(client, bufnr)
 end
 
 -- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 require('lspconfig')['pylsp'].setup {
   capabilities = capabilities,
   on_attach = on_attach
