c4c164c2a0d1555c86c91297ab3622f1ce12c396
ionutcalofir <ionut.calofir@gmail.com>
2024-01-20 01:48:37 +0100

    Update tmuxline color and fix airline plugin

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,7 +1,8 @@
 require('icalofir.plugins')
+-- For the airline plugin to work, it should be set before the colorscheme.
+require('icalofir.plugins_settings.airline')
 require('icalofir.settings')
 require('icalofir.mappings')
 require('icalofir.plugins_settings.nvim_cmp')
 require('icalofir.plugins_settings.treesitter')
-require('icalofir.plugins_settings.airline')
 require('icalofir.plugins_settings.indent_guides')
